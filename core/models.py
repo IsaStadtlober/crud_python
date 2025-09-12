d80cc44fe9468e22f986ce5ac3dc115022306955
Isabelle Stadtlober <isabellestadtlober@gmail.com>
2025-09-12 13:36:14 -0300

    ainda não está pronto o update, mas está melhor

--- core/models.py
+++ core/models.py
@@ -13,7 +13,7 @@ STATUS_CHOICES = [
 ]
 
 class Funcionario(models.Model):
-    matricula = models.IntegerField('Matrícula', primary_key=True)
+    matricula = models.IntegerField('Matrícula:', primary_key=True)
     nome = models.CharField(max_length=100, default='')
     funcao = models.CharField( max_length=100, verbose_name="Função", default='')
     status = models.CharField(max_length=2, choices=STATUS_CHOICES, default='A')
