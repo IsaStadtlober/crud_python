dfa761449ef75286b8c7b26348828c6dbe6d5303
Isabelle Stadtlober <isabellestadtlober@gmail.com>
2025-09-25 16:04:45 -0300

    página de login pronta

--- core/views.py
+++ core/views.py
@@ -88,7 +88,7 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user:
             login(request, user)
-            return redirect('index')  # Redireciona para /index/
+            return redirect('Listar')  # Redireciona para /index/
         else:
             messages.error(request, 'Usuário ou senha inválidos')
     return render(request, 'login.html')
